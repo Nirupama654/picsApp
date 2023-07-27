6169a42e31500643f3a96ac1cc1f0091727f81cf
Nirupama654 <prasadnirupama8@gmail.com>
2023-07-27 08:45:35 +0530

    Made some changes

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          PixieShots
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
