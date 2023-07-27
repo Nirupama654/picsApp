07859cb5cb1a055cb2786e94b82e7e4521524dc5
Nirupama654 <prasadnirupama8@gmail.com>
2023-07-27 07:29:32 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#FullStack Social Media App
+set up the uri for mongoDB cloud in .ENV file (in Server folder )
+
+In each of the folders Client as well as Server, run the following commands :
+npm install
+npm start
---
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,131 @@
+import {
+  ChatBubbleOutlineOutlined,
+  FavoriteBorderOutlined,
+  FavoriteOutlined,
+  ShareOutlined,
+} from "@mui/icons-material";
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import Modal from '@mui/material/Modal';
+import FlexBetween from "components/FlexBetween";
+import Friend from "components/Friend";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from "state";
+import SimpleModal from "./PictureOnly"
+import { useEffect } from "react";
+
+const PostWidget = ({
+  postId,
+  postUserId,
+  name,
+  description,
+  location,
+  picturePath,
+  userPicturePath,
+  likes,
+  comments,
+}) => {
+  const [isComments, setIsComments] = useState(false);
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.token);
+  const loggedInUserId = useSelector((state) => state.user._id);
+  const isLiked = Boolean(likes[loggedInUserId]);
+  const likeCount = Object.keys(likes).length;
+
+  const { palette } = useTheme();
+  const main = palette.neutral.main;
+  const primary = palette.primary.main;
+
+  const patchLike = async () => {
+    const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userId: loggedInUserId }),
+    });
+    const updatedPost = await response.json();
+    dispatch(setPost({ post: updatedPost }));
+  };
+
+  const [open, setOpen] = useState(false);
+  
+    const handleOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+
+
+    useEffect(() => {
+      
+    }, [open])
+    
+
+  return (
+    <div>
+      {open && <SimpleModal 
+      open={open} 
+      handleClose={handleClose} 
+      handleOpen={handleOpen} 
+      setOpen={setOpen}
+      postId={postId}
+      postUserId={postUserId}
+      name={name}
+      description={description}
+      location={location}
+      picturePath={picturePath}
+      userPicturePath={userPicturePath}
+      likes={likes}
+      comments={comments}
+      />}
+      {picturePath && (
+        <div className="image-grid" 
+        onClick={handleOpen}
+        >
+        <img
+          width="100%"
+          height="auto"
+          alt="post"
+          style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
+          src={`http://localhost:3001/assets/${picturePath}`}
+        />
+        <FlexBetween mt="0.25rem">
+        <FlexBetween gap="1rem">
+          <FlexBetween gap="0.3rem">
+            <IconButton onClick={patchLike}>
+              {isLiked ? (
+                <FavoriteOutlined sx={{ color: primary }} />
+              ) : (
+                <FavoriteBorderOutlined />
+              )}
+            </IconButton>
+            <Typography>{likeCount}</Typography>
+          </FlexBetween>
+
+          <FlexBetween gap="0.3rem">
+            <IconButton onClick={() => setIsComments(!isComments)}>
+              <ChatBubbleOutlineOutlined />
+            </IconButton>
+            <Typography>{comments.length}</Typography>
+          </FlexBetween>
+        </FlexBetween>
+
+        <IconButton>
+          <ShareOutlined />
+        </IconButton>
+      </FlexBetween>
+        </div>
+        
+      )}
+      
+      
+    </div>
+  );
+};
+
+export default PostWidget;
